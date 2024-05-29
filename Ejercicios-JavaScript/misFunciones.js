af66c7bd3c33a5cd73352c96f65525475629f76b
NicolasBono <2318599@ucc.edu.ar>
2024-05-29 09:12:38 -0300

    ejerciciosjava

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,15 +9,15 @@ let convertirunidades=(nombre, valor)=>{
     let metro,yarda,pie,pulgada;
     if (isNaN(valor)) {
         alert("el valor ingresado no es correcto ")
-        metro="";
-        pie="";
-        yarda="";
-        pulgada="";
+        metro= "";
+        pie= "";
+        yarda= "";
+        pulgada= "";
     } else if (nombre === "metro") {
-        metro=valor;
-        pulgada=valor * 39.3701;
-        pie = valor * 3.28084;
-        yarda= valor * 1.09361;
+        metro= valor;
+        pulgada= valor* 39.3701;
+        pie = valor* 3.28084;
+        yarda= valor* 1.09361;
     } else if (nombre === "pulgada") {
         pulgada=valor;
         metro= valor * 0.0254;
@@ -59,4 +59,25 @@ let mostrarocultar=(valor)=>{
     }else if (valor==="val_ocultar"){
         document.getElementById("unDiv").style.display='none';
     }
+}
+let suma=() => {
+    const s1=Number(document.getElementById("nums1").value);
+    const s2=Number(document.getElementById("nums2").value);
+    document.getElementById("totalS").value=s1+s2;
+}
+let resta=() => {
+    const s1=Number(document.getElementById("numr1").value);
+    const s2=Number(document.getElementById("numr2").value);
+    document.getElementById("totalR").value=s1-s2;
+}
+let multiplicar=() => {
+    const s1=Number(document.getElementById("numm1").value);
+    const s2=Number(document.getElementById("numm2").value);
+    document.getElementById("totalM").value=s1*s2;
+}
+
+let division=() => {
+    const s1=Number(document.getElementById("numd1").value);
+    const s2=Number(document.getElementById("numd2").value);
+    document.getElementById("totalD").value=s1/s2;
 }
