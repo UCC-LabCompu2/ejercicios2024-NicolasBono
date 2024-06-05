a2fdafcc8842250620c98ebe05c38e0821b60a6d
NicolasBono <2318599@ucc.edu.ar>
2024-06-05 08:51:37 -0300

    ejerciciosjava

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,82 +5,99 @@
  * @param {number}valor
  * @return Valor que retorna
  */
-let convertirunidades=(nombre, valor)=>{
-    let metro,yarda,pie,pulgada;
-    if (valor.includes(",")){
-        valor=valor.replace(",",".");
+let convertirunidades = (nombre, valor) => {
+    let metro, yarda, pie, pulgada;
+    if (valor.includes(",")) {
+        valor = valor.replace(",", ".");
     }
     if (isNaN(valor)) {
         alert("el valor ingresado no es correcto ")
-        metro= "";
-        pie= "";
-        yarda= "";
-        pulgada= "";
+        metro = "";
+        pie = "";
+        yarda = "";
+        pulgada = "";
     } else if (nombre === "metro") {
-        metro= valor;
-        pulgada= valor* 39.3701;
-        pie = valor* 3.28084;
-        yarda= valor* 1.09361;
+        metro = valor;
+        pulgada = valor * 39.3701;
+        pie = valor * 3.28084;
+        yarda = valor * 1.09361;
     } else if (nombre === "pulgada") {
-        pulgada=valor;
-        metro= valor * 0.0254;
-        pie= valor * 0.0833;
-       yarda= valor * 0.027777777777778;
+        pulgada = valor;
+        metro = valor * 0.0254;
+        pie = valor * 0.0833;
+        yarda = valor * 0.027777777777778;
     } else if (nombre === "pie") {
-        pie=valor;
-        metro=valor * 0.3048;
-       pulgada= valor * 12;
+        pie = valor;
+        metro = valor * 0.3048;
+        pulgada = valor * 12;
         yarda = valor * 0.333333333;
     } else if (nombre === "yarda") {
-        yarda= valor;
-        metro= valor * 0.9144;
-       pulgada = valor * 36;
-       pie= valor * 3;
+        yarda = valor;
+        metro = valor * 0.9144;
+        pulgada = valor * 36;
+        pie = valor * 3;
     }
     {
-        document.getElementById("metro").value = Math.round (metro*100)/100;
-        document.getElementById("pulgada").value = Math.round(pulgada*100)/100;
+        document.getElementById("metro").value = Math.round(metro * 100) / 100;
+        document.getElementById("pulgada").value = Math.round(pulgada * 100) / 100;
         document.getElementById("yarda").value = yarda.toFixed(2);
         document.getElementById("pie").value = pie.toFixed(2);
     }
 }
-function convertirgradosradianes(id){
-    let grad,rad;
-    if(id==="grados"){
-        let grad=document.getElementById("grados").value;
-        let rad=grad*Math.PI/180;
-        document.getElementById("radianes").value=rad;
-    }else if(id==="radianes"){
-        let rad=document.getElementById("radianes").value;
-        let grad= rad*180/Math.PI;
-        document.getElementById("grados").value= grad;
+
+function convertirgradosradianes(id) {
+    let grad, rad;
+    if (id === "grados") {
+        let grad = document.getElementById("grados").value;
+        let rad = grad * Math.PI / 180;
+        document.getElementById("radianes").value = rad;
+    } else if (id === "radianes") {
+        let rad = document.getElementById("radianes").value;
+        let grad = rad * 180 / Math.PI;
+        document.getElementById("grados").value = grad;
     }
-}
-let mostrarocultar=(valor)=>{
-    if(valor==="val_mostrar"){
-        document.getElementById("unDiv").style.display='block';
-    }else if (valor==="val_ocultar"){
-        document.getElementById("unDiv").style.display='none';
-    }
-}
-let suma=() => {
-    const s1=Number(document.getElementById("nums1").value);
-    const s2=Number(document.getElementById("nums2").value);
-    document.getElementById("totalS").innerHTML=s1+Number(s2);
-}
-let resta=() => {
-    const s1=Number(document.getElementById("numr1").value);
-    const s2=Number(document.getElementById("numr2").value);
-    document.getElementById("totalR").innerHTML=s1-s2;
-}
-let multiplicar=() => {
-    const s1=Number(document.getElementById("numm1").value);
-    const s2=Number(document.getElementById("numm2").value);
-    document.getElementById("totalM").innerHTML=s1*s2;
 }
 
-let division=() => {
-    const s1=Number(document.getElementById("numd1").value);
-    const s2=Number(document.getElementById("numd2").value);
-    document.getElementById("totalD").innerHTML=s1/s2;
+let mostrarocultar = (valor) => {
+    if (valor === "val_mostrar") {
+        document.getElementById("unDiv").style.display = 'block';
+    } else if (valor === "val_ocultar") {
+        document.getElementById("unDiv").style.display = 'none';
+    }
+}
+let suma = () => {
+    const s1 = Number(document.getElementById("nums1").value);
+    const s2 = Number(document.getElementById("nums2").value);
+    document.getElementById("totalS").innerHTML = s1 + Number(s2);
+}
+let resta = () => {
+    const s1 = Number(document.getElementById("numr1").value);
+    const s2 = Number(document.getElementById("numr2").value);
+    document.getElementById("totalR").innerHTML = s1 - s2;
+}
+let multiplicar = () => {
+    const s1 = Number(document.getElementById("numm1").value);
+    const s2 = Number(document.getElementById("numm2").value);
+    document.getElementById("totalM").innerHTML = s1 * s2;
+}
+
+let division = () => {
+    const s1 = Number(document.getElementById("numd1").value);
+    const s2 = Number(document.getElementById("numd2").value);
+    document.getElementById("totalD").innerHTML = s1 / s2;
+}
+
+function dibujarcirculocuadrado() {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+    const tamnio=200;
+    const alturamax=canvas.height;
+    const anchomax=canvas.width;
+    const margen=10;
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(margen, alturamax-tamnio-margen, tamnio, tamnio);
+    ctx.fillStyle="blue";
+    ctx.arc(anchomax/2,alturamax/2,tamnio/2,0,2*Math.PI );
+    ctx.stroke();
+    ctx.fill();
 }
